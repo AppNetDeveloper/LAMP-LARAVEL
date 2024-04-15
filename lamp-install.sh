9f7dc1ee4324f5f0398d5b4e09f570f6de523f85
Liviu diaconu <liviudiaconu@appnet.dev>
2024-04-15 11:06:11 +0200

    Update lamp-install.sh

--- lamp-install.sh
+++ lamp-install.sh
@@ -1,5 +1,37 @@
 #!/bin/sh
 cd ~ || exit
+
+
+# Obtener la arquitectura de la CPU
+ARCH=$(uname -m)
+
+if [ "$ARCH" = "x86_64" ]; then
+  echo "**Añadiendo repositorios Debian nonfree para x86_64 (Debian 12)**"
+
+  # Añadir la línea "deb http://deb.debian.org/debian bookworm main contrib non-free" al archivo /etc/apt/sources.list
+  echo "deb http://deb.debian.org/debian bookworm main contrib non-free" | sudo tee -a /etc/apt/sources.list
+
+  # Actualizar la lista de paquetes
+  sudo apt update
+
+elif [ "$ARCH" = "aarch64" ]; then
+  echo "**Añadiendo repositorios Debian nonfree para aarch64 (Debian 12)**"
+
+  # Añadir la línea "deb http://deb.debian.org/debian bookworm main contrib non-free"  al archivo /etc/apt/sources.list
+  echo "deb http://deb.debian.org/debian bookworm main contrib non-free" | sudo tee -a /etc/apt/sources.list
+echo "deb [arch=armhf] http://httpredir.debian.org/debian/ buster main contrib non-free" | sudo tee -a /etc/apt/sources.list
+  # Actualizar la lista de paquetes
+  sudo apt update
+
+else
+  echo "**Arquitectura de CPU no compatible: $ARCH**"
+  echo "Este script solo funciona en sistemas x86_64 o aarch64."
+  exit 1
+fi
+
+echo "**Repositorios Debian nonfree añadidos correctamente (Debian 12)**"
+
+
 curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 

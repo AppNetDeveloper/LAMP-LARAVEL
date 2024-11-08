cb8b5ba0c7a4ae95599cba99ca7bfe47f0c69c24
Liviu diaconu <liviudiaconu@appnet.dev>
2024-11-08 13:41:05 +0100

    main

--- lamp-install.sh
+++ lamp-install.sh
@@ -1194,10 +1194,27 @@ sudo apt install -y  mosquitto-clients
 sudo apt-get -y install erlang 
 sudo apt -y  install libsnappy1v5
 
-wget https://github.com/vernemq/vernemq/releases/download/2.0.1/vernemq-2.0.1.jammy.x86_64.deb
-wget  https://github.com/vernemq/vernemq/releases/download/2.0.1/vernemq-2.0.1.jammy.arm64.deb
-sudo dpkg -i  vernemq-2.0.1.jammy.x86_64.deb
-sudo dpkg -i  vernemq-2.0.1.jammy.arm64.deb
+# Detecta la arquitectura
+ARCH=$(uname -m)
+
+# Define las URLs para cada arquitectura
+URL_X86="https://github.com/vernemq/vernemq/releases/download/2.0.1/vernemq-2.0.1.jammy.x86_64.deb"
+URL_ARM="https://github.com/vernemq/vernemq/releases/download/2.0.1/vernemq-2.0.1.jammy.arm64.deb"
+
+# Descarga y instala según la arquitectura detectada
+if [ "$ARCH" == "x86_64" ]; then
+    wget $URL_X86 -O vernemq.deb
+    sudo dpkg -i vernemq.deb
+elif [ "$ARCH" == "aarch64" ]; then
+    wget $URL_ARM -O vernemq.deb
+    sudo dpkg -i vernemq.deb
+else
+    echo "Arquitectura no soportada: $ARCH"
+    exit 1
+fi
+
+# Limpia el archivo descargado
+rm -f vernemq.deb
 
 # Configurar el archivo de configuración de vernemq
 echo "Configurando VerneMQ."
